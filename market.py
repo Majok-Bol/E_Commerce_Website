f390cbe019d96d97228dbad220782b371f19f58b
Majok-Bol <bolmajok001@gmail.com>
2024-12-03 00:36:25 +0300

    added market.py file

--- market.py
+++ market.py
@@ -1,10 +1,20 @@
 '''Import flask class from Flask module'''
-from flask import Flask
+from flask import Flask,render_template
 '''Create instance of flask class'''
 app=Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+@app.route('/market')
+def market_page():
+     items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+     
+     return render_template('market.html',item_values=items)
 
 if __name__=='__main__':
-   app.run()
+   app.run(debug=True)
