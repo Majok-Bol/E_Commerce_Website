8e2fbf67245f678285463ca318eac71a1a828b84
Majok-Bol <bolmajok001@gmail.com>
2024-12-06 23:15:49 +0300

    added user ownership of items

--- market/models.py
+++ market/models.py
@@ -3,10 +3,10 @@ from market import db
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username=db.Column(db.String(length=30),unique=True,nullable=False)
-    email_addess=db.Column(db.String(length=50),nullable=False,unique=True)
+    email_address=db.Column(db.String(length=50),nullable=False,unique=True)
     password_hash=db.Column(db.String(length=60),nullable=False)
-    budget=db.Column(db.Integer(),nullable=False,default=1000)
-    items=db.relationship('Item',backref='owned_usr',lazy=True)
+    budget=db.Column(db.Integer(),default=1000)
+    items=db.relationship('Item',backref='owned_user',lazy=True)
 #create instance of database
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
